04cf12f8c8a94b0853e82fbe6077a853ee7c480e
K-JhansiBai <142075249+K-JhansiBai@users.noreply.github.com>
2023-09-30 10:56:36 +0530

    Update Exp11.js

--- Exp11.js
+++ Exp11.js
@@ -1,29 +1,32 @@
-import React,{useState,useEffect} from "react";
-function Exp11(){
-    const [mytime,setTime]=useState("");
-    const tick=()=>{
-        let time=new Date().getHours()+":"+new Date().getMinutes()+":"+new Date().getSeconds;
-        if(new Date.getHours()<12){
-            time=time+"AM"
-        }
-        else{
-            time=time+"PM"
-            setTime(time);
-        }
+import React, { useState, useEffect } from 'react';
+
+function DigitalClock(){
+  const [mytime, setTime] = useState('');
+  const tick=()=>{
+    let time = new Date().getHours()+": "+new Date().getMinutes()+": ".padStart(2,'0')+ new Date().getSeconds();
+    if (new Date().getHours()<12)
+    {
+        time = time + " AM"
     }
-    useEffeect(()=>{
-        const t=setInterval(tick,1000);
-        return()=>{
-            clearTimeout(t);
-        }
-    },[mytime])
-    return(
-        <center>
-            <div>
-                <h1>Digital Clock</h1>
-                <h1>{mytime}</h1>
-            </div>
-        </center>
-    );
-}
-export default Exp11;
+    else{
+        time = time + " PM"
+    }
+    setTime(time);
+  };
+
+  useEffect(() => {
+    const t = setInterval(tick,1000);
+    return ()=>{
+        clearInterval(t);
+    }
+  },[mytime])
+
+  return (
+    <div>
+      <h1>Digital Clock</h1>
+      <h2>{mytime}</h2>
+    </div>
+  );
+  }
+
+export default DigitalClock;
